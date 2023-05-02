7af0c740b9d66304adb78537f6b7b1dceed7e842
AhmadNaufal2525 <ahmadnaufal588@gmail.com>
2023-05-02 21:36:35 +0700

    add terdekatku and popular page

---
+++ src/routes/terdekatkuPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/navbar/navbar'
+import Terdekatku from '../pages/terdekatku';
+import Footer from '../components/footer/footer'
+const TerdekatkuPage = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Terdekatku/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default TerdekatkuPage
